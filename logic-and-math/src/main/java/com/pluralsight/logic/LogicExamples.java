8bf453681723c48e1f6044bed3b58bfe81ab4b63
Siry Siu <sewsiri@gmail.com>
2024-10-01 15:17:18 -0400

    logic and math exercise

--- logic-and-math/src/main/java/com/pluralsight/logic/LogicExamples.java
+++ logic-and-math/src/main/java/com/pluralsight/logic/LogicExamples.java
@@ -1,7 +1,63 @@
 package com.pluralsight.logic;
 
 public class LogicExamples {
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        double area = (double) 10 / 3;
+
+        double remainder = 10 % 3;
+
+        System.out.println(area);
+        System.out.println(remainder);
+
+        // Arithmetic operators
+        // + -  * / %
+        // Increment/ Decrement operators
+        //++ --
+
+        area = area + 1;
+        System.out.println(area);
+
+        area ++;
+        System.out.println(area);
+
+        //System.out.println(area++);
+
+        System.out.println(++ area);
+
+        double z = 8.33f;
+        System.out.println(z);
+
+        System.out.printf("%.2f\n", z);
+        System.out.println(z);
+
+        // Comparison operators
+        boolean amIDone = false;
+        int counter = 0;
+
+        do {
+            System.out.println("talking... words..");
+
+            if (counter++ == 5) amIDone = true;
+        } while (! amIDone);
+
+        System.out.println(counter);
+
+        counter =0;
+        while( counter != 5) {
+            System.out.println("talking... words...");
+            counter ++;
+        }
+
+
+        for (int i = 0; i < 5; i++ ) {
+            System.out.println("talking..");
+        }
+
+        if (counter > 0) {
+            System.out.println("we talked about it");
+        }
     }
 }
