52691a5726d7b8cd7803dd4c4578f1217e405202
Siry Siu <sewsiri@gmail.com>
2024-10-07 15:38:39 -0400

    while loops

---
+++ while-loop/src/main/java/com/pluralsight/loops/DoWhileLoop.java
@@ -0,0 +1,11 @@
+package com.pluralsight.loops;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println("I love Java");
+            i++;
+        } while (i < 5);
+    }
+}
---
+++ while-loop/src/main/java/com/pluralsight/loops/WhileLoops.java
@@ -0,0 +1,11 @@
+package com.pluralsight.loops;
+
+public class WhileLoops {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 5) {
+            System.out.println("I love Java");
+            i++;
+        }
+    }
+}
